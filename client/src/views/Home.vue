297f5b65f76f1f27ff7e32c5a9b31ffa382a7028
MoritzKraus <moritz.kraus@gmx.net>
2020-11-07 19:14:51 +0100

    New logo

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/mevn.jpg" width="350" />
+    <img alt="communify logo" src="../assets/communify.png" width="350" />
     <HelloWorld msg="Default Boilerplate Template" />
   </div>
 </template>
